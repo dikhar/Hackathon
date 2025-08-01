fc2ae3b0c17c2271b1b10a034d9753689a1986d9
Shivansh Vaish <shivansh.vaish@telekom-digital.com>
2025-08-01 13:10:11 +0530

    NotificationScheduler

---
+++ src/main/java/com/smartcart/service/SmsService.java
@@ -0,0 +1,35 @@
+package com.smartcart.service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsService {
+
+    @Value("${twilio.accountSid}")
+    private String accountSid;
+
+    @Value("${twilio.authToken}")
+    private String authToken;
+
+    @Value("${twilio.fromNumber}")
+    private String fromNumber;
+
+    @PostConstruct
+    public void init() {
+        Twilio.init(accountSid, authToken);
+    }
+
+    public void sendSms() {
+        Message.creator(
+            new PhoneNumber("+1234567890"), // Replace with the recipient's phone number
+            new PhoneNumber("+0987654321"), // Replace with your Twilio phone number
+            "message"
+        ).create();
+    }
+}
+
